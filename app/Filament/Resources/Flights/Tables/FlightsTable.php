3591f198fea86e8612a5898dee25555f40d23022
juancano <juanjosecf21@gmail.com>
2026-09-16 15:46:54 -0500

    Actualización resources

--- app/Filament/Resources/Flights/Tables/FlightsTable.php
+++ app/Filament/Resources/Flights/Tables/FlightsTable.php
@@ -6,6 +6,7 @@ use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Tables\Table;
+use Filament\Tables\Columns\TextColumn;
 
 class FlightsTable
 {
@@ -13,7 +14,32 @@ class FlightsTable
     {
         return $table
             ->columns([
-                //
+               TextColumn::make('airline')
+            ->label('Aerolínea')
+            ->searchable(),
+
+                TextColumn::make('origin')
+            ->label('Origen'),
+
+                TextColumn::make('destination')
+            ->label('Destino'),
+
+                TextColumn::make('departure_date')
+            ->label('Fecha')
+            ->date(),
+
+                TextColumn::make('departure_time')
+            ->label('Hora'),
+
+                TextColumn::make('seats')
+            ->label('Cupos'),
+
+                TextColumn::make('price')
+            ->label('Precio'),
+
+                TextColumn::make('status')
+            ->label('Estado')
+            ->badge(),
             ])
             ->filters([
                 //
